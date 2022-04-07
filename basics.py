ca55003cd00734ce3555e10a3fed8d9074e02c2a
ProfessorMkGee <89929145+ProfessorMkGee@users.noreply.github.com>
2022-04-07 14:59:06 -0400

    Add files via upload

---
+++ basics.py
@@ -0,0 +1,33 @@
+#Give a thumbs up or type ready when you are good
+
+#legal variables
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+
+#illegal variables --> Why do these not work???
+#2myvar = "John" #numbers are no good in variable names
+#my-var = "John" #hyphens are not good!
+#my var = "John" #spacing is no good according python
+
+#unpack a list
+fruits = ["apple", "banana", "cherry"]
+#set x to apple
+#set y to banana
+#set z to cherry
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+
+#Python Numbers
+x = 1    # int - whole number
+y = 2.8  # float - deals with decimals
+z = 1j   # complex - can be symbolic
+
+print(type(x))
+print(type(y))
+print(type(z))
+
